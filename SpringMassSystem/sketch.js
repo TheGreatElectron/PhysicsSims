d683e601f9be176de00a41b5d2065e2674a63a4b
Adrian Logan <64bitadrian@gmail.com>
2016-02-19 12:54:20 -0500

    Replaced spring
    Replaced the spring function with the spring object (from Hedberg).

--- SpringMassSystem/sketch.js
+++ SpringMassSystem/sketch.js
@@ -1,21 +1,19 @@
 //Background image
 var bg;
-//Sliders
-var mSlider, kSlider, aSlider;
 //Buttons
 var playButton;
 //bool to toggle animation
 var play;
 
-//User defined variables
+//Sliders
+var mSlider, kSlider, aSlider;
+//Slider defined variables
 var mass, springk, amplitude;
-//Calculated variables
-var omega, period, velocity, acceleration, position;
+//calculated variable
+var vel, acc;
 
-//pixel position on X
-var xpos;
-//Time Variables
-var t, tzero;
+//spring
+var spring;
 
 
 
@@ -31,10 +29,6 @@ function setup() {
   springk = 6;
   amplitude = 2;
   
-  //set time
-  tzero = millis()/1000;
-  t = 0;
-  
   //set play
   play = true;
   
@@ -45,9 +39,11 @@ function setup() {
   mSlider = createSlider(1, 21, mass);
   mSlider.position(20, 310);
   kSlider = createSlider(1, 11, springk);
-  kSlider.position(20, 360);
-  aSlider = createSlider(-4, 4, amplitude);
-  aSlider.position(165, 310);
+  kSlider.position(165, 310);
+  aSlider = createSlider(-10, 10, amplitude);
+  aSlider.position(310, 310);
+  
+  spring = new Spring(createVector(20,179),springk,mass,308,amplitude/10);
 
 }
 
@@ -55,8 +51,8 @@ function draw() {
   //Draw Basics
   background(bg);
   text("Mass: "+mass+" kg", 20, 305);
-  text("Spring Constant: "+springk+" N/m", 20, 355);
-  text("Amplitude: "+amplitude+" m", 165, 305);
+  text("Spring K: "+springk+" N/m", 165, 305);
+  text("Amplitude: "+(amplitude/2)+" m", 310, 305);
   
   //Draw number lines
   fill(255);
@@ -70,83 +66,43 @@ function draw() {
     text(round(i*10-5),xpos-4,260);
   }
   
-  //Reset time if Amplitude changes
-  if (amplitude != aSlider.value()){
-    resetTimer();
-  }
   
-  //update vars based on sliders
-  mass = mSlider.value();
-  springk = kSlider.value();
-  amplitude = aSlider.value();
+  //Update Variables and reset if changed
+  if (updateVars()){
+    spring = new Spring(createVector(20,179),springk,mass,308,amplitude/10);
+  }
 
   //Toggle animation
   if (play) {
-    //update time
-    timer();
+    spring.update();
   }
-  else {
-    holdTime();
-  }
-  calculateVar(); 
-  
-  //Draw Box
-  fill(map(mass,1,21,205,100));
-  stroke(0);
-  xpos = map(position,-5,5,67,590);
-  rect(xpos-50,129,100,100); 
-  
   //draw spring
-  drawSpring(20,xpos-50,179);
+  spring.display();
+  
+  //calculate vel and acc
+  vel = round(spring.velocity.x);
+  acc = round(spring.acceleration.x);
+  
+  //type vel and acc
+  text("Velocity: "+vel+" ", 77, 20);
+  text("Acceleration: "+acc+" ", 237, 20);
+}
+
+function updateVars(){
+  //updates slider variables and returns true if changed
+  var hasChanged = false;
+  if (amplitude != aSlider.value() || mass != mSlider.value() || springk != kSlider.value()){
+    hasChanged = true;
+  }
+  
+  mass = mSlider.value();
+  springk = kSlider.value();
+  amplitude = aSlider.value();
+  
+  return hasChanged;
 }
 
 function togglePlay(){
   play = !play;
 }
 
-function timer(){
-  t=millis()/1000-tzero;
-}
-function holdTime() {
-  tzero=millis()/1000-t;
-}
-
-function resetTimer(){
-  tzero=millis()/1000;
-  t=0;
-}
-
-function calculateVar() {
-  //Calculate dependant variables
-  omega = sqrt(springk/mass);
-  period = TWO_PI/omega;
-  position = amplitude*cos(omega*t);
-}
-
-function drawSpring(x1,x2,y) {
-  strokeWeight(4);
-  stroke(0);
-  var x = [];
-  var xi, xf;
-  var flip=false;
-  for(var i=0;i<=20;++i){
-    x[i] = lerp(x1,x2,i/20);
-  }
-  //Draw front end
-  line(x1,y,(x[0]+x[1])/2,y-20);
-  //Draw middle spring
-  for(var i=1;i<=20;++i){
-    xi = (x[i-1]+x[i])/2;
-    xf = (x[i]+x[i+1])/2;
-    if(flip){
-      line(xi,y+20,xf,y-20);
-      flip=!flip;
-    }
-    else{
-      line(xf,y+20,xi,y-20);
-      flip=!flip;    
-    }
-  }
-  //Draw back end
-  line((x[19]+x[20])/2,y+20,x2,y);
-}
